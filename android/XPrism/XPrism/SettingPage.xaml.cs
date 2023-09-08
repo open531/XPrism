5a524bacd73e82cca903f92bca7ede781fac6fc3
Panxuc <panxuc0618@gmail.com>
2023-09-08 08:17:37 +0800

    feat: :sparkles: AP

--- android/XPrism/XPrism/SettingPage.xaml.cs
+++ android/XPrism/XPrism/SettingPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Net.NetworkInformation;
+
 namespace XPrism;
 
 public partial class SettingPage : ContentPage
@@ -9,6 +11,7 @@ public partial class SettingPage : ContentPage
         InitializeComponent();
         picoIPEntry.Text = ESP32Info.picoIPAddress;
         camIPEntry.Text = ESP32Info.camIPAddress;
+        GetIP();
     }
     public bool isValidIP(string ip)
     {
@@ -42,6 +45,44 @@ public partial class SettingPage : ContentPage
         else
         {
             DisplayAlert("IP无效", "请输入有效的IP地址", "OK");
+        }
+    }
+    public void GetIP()
+    {
+        Boolean hasAP = false;
+        String APIP = "";
+        NetworkInterface[] nis = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface ni in nis)
+        {
+            //IPInterfaceProperties ipps = ni.GetIPProperties();
+            //UnicastIPAddressInformationCollection ipcs = ipps.UnicastAddresses;
+            //foreach (UnicastIPAddressInformation ipc in ipcs)
+            //{
+            //    Console.WriteLine("IP: " + ipc.Address.ToString() + ", Interface: " + ni.Name);
+            //}
+            if (ni.Name == "ap0")
+            {
+                hasAP = true;
+                IPInterfaceProperties ipps = ni.GetIPProperties();
+                UnicastIPAddressInformationCollection ipcs = ipps.UnicastAddresses;
+                foreach (UnicastIPAddressInformation ipc in ipcs)
+                {
+                    if (ipc.Address.ToString().IndexOf('.') != -1)
+                    {
+                        APIP = ipc.Address.ToString();
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        if (hasAP)
+        {
+            suggestLabel.Text = "您似乎已打开热点😊\n热点的IP地址为：\n" + APIP + "\n供您参考。";
+        }
+        else
+        {
+            suggestLabel.Text = "您似乎未打开热点🤔\n建议您打开热点以便于连接。";
         }
     }
 }
